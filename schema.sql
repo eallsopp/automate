b25876292860e29186ba3dd61ddbd005f97952f6
Erik Allsopp <erik.allsopp@gmail.com>
2020-12-19 17:48:04 -0800

    modified schema

--- schema.sql
+++ schema.sql
@@ -8,5 +8,6 @@ CREATE TABLE activities (
   id serial PRIMARY KEY,
   activity_name text NOT NULL,
   minutes_used integer NOT NULL,
+  date date NOT NULL default CURRENT_DATE,
   user_id integer REFERENCES users (id)
 );
